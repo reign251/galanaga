5721bb3bfebc1a7e025c365dd14b8e0d5317c6ec
reign251 <reginegalanaga8@gmail.com>
2020-10-17 21:55:49 +0800

    first commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main (String [] args) {
+        System.out.println ("This Application Displays A Box, An Oval, An Arrow"
+                + " And A Diamond Using Asterisks (*)");
+        System.out.print("                                                                          *               \n");                                   
+        System.out.print ("* * * * * * * * *                                     *                *   *             \n");
+        System.out.print ("*               *        * * * * *                      *            *       *           \n");
+        System.out.print ("*               *      *           *                     *         *           *         \n");
+        System.out.print ("*               *     *             *     * * * * * * * * *          *       *           \n");
+        System.out.print ("*               *      *           *                     *             *   *             \n");
+        System.out.print ("* * * * * * * * *        * * * * *                      *                *               \n");
+        System.out.print("                                                       *                                  \n");
+    } 
+}
